57cf612d4a663fba4d198f8a4dbfbdd052425662
vahaponur <vahaponuryildirim@gmail.com>
2022-03-05 16:35:20 +0300

    Refactoring
    Code cleaning

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -11,35 +11,39 @@ public class Movement : MonoBehaviour
     [SerializeField] private float _thrustForce = 1000f;
     [SerializeField] private float _rotationSpeed = 50f;
     [SerializeField] private KeyCode _thrushtKey = KeyCode.Space;
-    [SerializeField] private KeyCode _leftRotateKey = KeyCode.D;
-    [SerializeField] private KeyCode _rightRotateKey = KeyCode.A;
+    [SerializeField] private KeyCode _leftRotateKey = KeyCode.A;
+    [SerializeField] private KeyCode _rightRotateKey = KeyCode.D;
     [SerializeField] private AudioClip _thrustSFX;
     [SerializeField] private ParticleSystem _thrushtParticle;
     [SerializeField] private ParticleSystem _leftBooster;
     [SerializeField] private ParticleSystem _rightBooster;
+
     #endregion
 
     #region Private Fields
+
     /// <summary>
     /// Rigidbody of the attached gameobject
     /// </summary>
     private Rigidbody _rigidbody;
-    
+
     /// <summary>
     /// Audio source attached to gameobject
     /// </summary>
     private AudioSource _audioSource;
+
     #endregion
 
     #region Public Properties
+
     #endregion
 
     #region MonoBehaveMethods
+
     void Awake()
     {
         _rigidbody = this.GetComponent<Rigidbody>();
         _audioSource = this.GetComponent<AudioSource>();
-
     }
 
     void Update()
@@ -47,52 +51,47 @@ public class Movement : MonoBehaviour
         ProcessThrust();
         ProcessRotation();
     }
-    
+
     #endregion
-    
+
     #region PublicMethods
+
     #endregion
-    
+
     #region PrivateMethods
-    
+
     /// <summary>
     /// Process the rockets thrust by given inputs 
     /// </summary>
     private void ProcessThrust()
     {
-        _audioSource.PlayOnGetKey(_thrushtKey,_thrustSFX);
-        _thrushtParticle.PlayOnGetKey(_thrushtKey);
-        
         if (Input.GetKey(_thrushtKey))
         {
-         
-            _rigidbody.AddRelativeForce(Vector3.up* _thrustForce* Time.deltaTime );
+            StartThrusting();
         }
-    
-        
+        else
+        {
+            StopThrushting();
+        }
     }
-    
+
     /// <summary>
     /// Process rockets rotation by given inputs
     /// </summary>
     void ProcessRotation()
     {
-        _rightBooster.PlayOnGetKey(_rightRotateKey);
-        _leftBooster.PlayOnGetKey(_leftRotateKey);
-        
         if (Input.GetKey(_rightRotateKey))
         {
-            ApplyRotation(_rotationSpeed);
+            StartRightRotation();
             return;
         }
 
         if (Input.GetKey(_leftRotateKey))
         {
-            ApplyRotation(-_rotationSpeed);
+            StartLeftRotation();
         }
-       
     }
-    
+
     /// <summary>
     /// Applies rotation with given speed on Z axis
     /// </summary>
@@ -101,10 +100,44 @@ public class Movement : MonoBehaviour
     {
         _rigidbody.freezeRotation = true;
         transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
-        _rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezePositionZ;
-
+        _rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY |
+                                 RigidbodyConstraints.FreezePositionZ;
     }
     
+    /// <summary>
+    /// Sequence of events when starting thrusting
+    /// </summary>
+    void StartThrusting()
+    {
+        _audioSource.PlayOnGetKey(_thrushtKey, _thrustSFX);
+        _thrushtParticle.PlayOnGetKey(_thrushtKey);
+        _rigidbody.AddRelativeForce(Vector3.up * _thrustForce * Time.deltaTime);
+    }
     
+    /// <summary>
+    /// Sequence of events when thrusting stopped
+    /// </summary>
+    void StopThrushting()
+    {
+    }
+    
+    /// <summary>
+    /// Sequence of events in case player starts to right rotation
+    /// </summary>
+    void StartRightRotation()
+    {
+        _leftBooster.PlayOnGetKey(_rightRotateKey);
+        ApplyRotation(-_rotationSpeed);
+    }
+    
+    /// <summary>
+    /// Sequence of events in case player starts to left rotation
+    /// </summary>
+    void StartLeftRotation()
+    {
+        _rightBooster.PlayOnGetKey(_leftRotateKey);
+        ApplyRotation(_rotationSpeed);
+    }
+
     #endregion
 }
